c7a1574ecbd5514eb97d8b1fe11782a7cdfb148a
Javier Rodriguez Posada <jrposada.dev@gmail.com>
2022-04-01 20:11:18 +0200

    Complete merge from main

--- frontend/src/app-layout/AppBody.tsx
+++ frontend/src/app-layout/AppBody.tsx
@@ -1,6 +1,5 @@
 import { Route, Switch } from 'react-router-dom'
 
-import CreateRecipe from './pages/create-recipe/create-recipe'
 import Home from './pages/home'
 import Recipes from './pages/recipes'
 import User from './pages/user'
@@ -13,7 +12,6 @@ function AppBody() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/recipes" component={Recipes} />
-        <Route exact path="/create-recipe" component={CreateRecipe} />
         <Route exact path="/user" component={User} />
       </Switch>
     </div>
